84d611f254e8e89aa72cb50e61c70b7304362982
Sanefoxx <alexndr.lisitsin@gmail.com>
2018-05-30 23:48:09 +0300

    created new map_storages and unit tests of them

---
+++ src/com/sanefox/webapp/storage/MapResumeStorage.java
@@ -0,0 +1,57 @@
+package com.sanefox.webapp.storage;
+
+import com.sanefox.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapResumeStorage extends AbstractStorage {
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public List<Resume> doCopyAll() {
+        return new ArrayList<>(map.values());
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object resume) {
+        map.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected void doSave(Resume r, Object resume) {
+        map.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected Resume doGet(Object resume) {
+        return (Resume) resume;
+    }
+
+    @Override
+    protected void doDelete(Object resume) {
+        map.remove(((Resume) resume).getUuid());
+    }
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return map.get(uuid);
+    }
+
+    @Override
+    protected boolean isExist(Object resume) {
+        return resume != null;
+    }
+}
---
+++ test/com/sanefox/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,8 @@
+package com.sanefox.webapp.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest{
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
