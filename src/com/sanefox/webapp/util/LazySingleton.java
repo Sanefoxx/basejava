69a537638881501d4a6c72474d8586fbabd90ea3
Sanefoxx <alexndr.lisitsin@gmail.com>
2018-09-11 17:08:48 +0300

    ls11_lazysingleton

---
+++ src/com/sanefox/webapp/util/LazySingleton.java
@@ -0,0 +1,27 @@
+package com.sanefox.webapp.util;
+
+/**
+ * Created by aslisicin on 11.09.2018.
+ */
+public class LazySingleton {
+    volatile private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+    }
+}
