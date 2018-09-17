db454c2f3e056261497bacfa42c20e20e78499c9
Sanefoxx <alexndr.lisitsin@gmail.com>
2018-09-17 20:39:11 +0300

    ls15_hw14

--- test/com/sanefox/webapp/storage/AbstractStorageTest.java
+++ test/com/sanefox/webapp/storage/AbstractStorageTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.io.File;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,15 +25,13 @@ public abstract class AbstractStorageTest {
     protected Storage storage;
 
     private static final String UUID_1 = UUID.randomUUID().toString();
-    private static final Resume R1;
-
     private static final String UUID_2 = UUID.randomUUID().toString();
-    private static final Resume R2;
-
     private static final String UUID_3 = UUID.randomUUID().toString();
-    private static final Resume R3;
-
     private static final String UUID_4 = UUID.randomUUID().toString();
+
+    private static final Resume R1;
+    private static final Resume R2;
+    private static final Resume R3;
     private static final Resume R4;
 
     static {
@@ -40,9 +39,13 @@ public abstract class AbstractStorageTest {
         R2 = new Resume(UUID_2, "Name2");
         R3 = new Resume(UUID_3, "Name3");
         R4 = new Resume(UUID_4, "Name4");
-/*
+
         R1.addContact(ContactType.MAIL, "mail1@ya.ru");
         R1.addContact(ContactType.PHONE, "11111");
+
+        R4.addContact(ContactType.PHONE, "44444");
+        R4.addContact(ContactType.SKYPE, "Skype");
+/*
         R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
         R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
         R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
@@ -63,7 +66,8 @@ public abstract class AbstractStorageTest {
         R1.addSection(SectionType.EXPERIENCE,
                 new OrganizationSection(
                         new Organization("Organization2", "http://Organization2.ru",
-                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));*/
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+*/
     }
 
     protected AbstractStorageTest(Storage storage) {
@@ -91,7 +95,10 @@ public abstract class AbstractStorageTest {
 
     @Test
     public void update() throws Exception {
-        Resume newResume = new Resume(UUID_1, "New name");
+        Resume newResume = new Resume(UUID_1, "New Name");
+        R1.addContact(ContactType.MAIL, "mail1@google.com");
+        R1.addContact(ContactType.SKYPE, "NewSkype");
+        R1.addContact(ContactType.MOBILE, "+7 921 222-22-22");
         storage.update(newResume);
         assertTrue(newResume.equals(storage.get(UUID_1)));
     }
@@ -99,6 +106,15 @@ public abstract class AbstractStorageTest {
     @Test(expected = NotExistStorageException.class)
     public void updateNotExist() throws Exception {
         storage.get("dummy");
+    }
+
+    @Test
+    public void getAllSorted() throws Exception {
+        List<Resume> list = storage.getAllSorted();
+        assertEquals(3, list.size());
+        List<Resume> sortedResumes = Arrays.asList(R1, R2, R3);
+        Collections.sort(sortedResumes);
+        assertEquals(sortedResumes, list);
     }
 
     @Test
@@ -111,18 +127,6 @@ public abstract class AbstractStorageTest {
     @Test(expected = ExistStorageException.class)
     public void saveExist() throws Exception {
         storage.save(R1);
-    }
-
-    @Test
-    public void get() throws Exception {
-        assertGet(R1);
-        assertGet(R2);
-        assertGet(R3);
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void getNotExist() throws Exception {
-        storage.get("dummy");
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -138,17 +142,22 @@ public abstract class AbstractStorageTest {
     }
 
     @Test
-    public void getAllSorted() throws Exception {
-        List<Resume> list = storage.getAllSorted();
-        assertEquals(3, list.size());
-        assertEquals(Arrays.asList(R1, R2, R3), list);
+    public void get() throws Exception {
+        assertGet(R1);
+        assertGet(R2);
+        assertGet(R3);
     }
 
-    private void assertSize(int num) {
-        assertEquals(num, storage.size());
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
     }
 
     private void assertGet(Resume r) {
         assertEquals(r, storage.get(r.getUuid()));
+    }
+
+    private void assertSize(int size) {
+        assertEquals(size, storage.size());
     }
 }
