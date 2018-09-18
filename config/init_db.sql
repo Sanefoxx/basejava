02a257aa53dc965b5fc74f9f344dd2ca24009733
Sanefoxx <alexndr.lisitsin@gmail.com>
2018-09-18 22:51:10 +0300

    ls16_hw15_db_+++

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,27 +1,26 @@
-create table resume
-(
-  uuid      char(36) not null
-    constraint resume_pkey
-    primary key,
-  full_name varchar    not null
+
+CREATE TABLE resume (
+  uuid      CHAR(36) PRIMARY KEY NOT NULL,
+  full_name TEXT                 NOT NULL
 );
 
-create table contact
-(
-  id          serial   not null
-    constraint contact_pkey
-    primary key,
-  resume_uuid char(36) not null
-    constraint contact_resume_uuid_fk
-    references resume
-    on delete cascade,
-  type        varchar     not null,
-  value       varchar     not null
-
+CREATE TABLE contact (
+  id          SERIAL,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
 );
+CREATE UNIQUE INDEX contact_uuid_type_index
+  ON contact (resume_uuid, type);
 
-create unique index contact_uuid_type_index
-  on contact (resume_uuid, type);
+CREATE TABLE section (
+  id          SERIAL PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+  ON section (resume_uuid, type);
 
 
 
